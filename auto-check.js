bb870728fc6bc20d0fa471d523f65b83b46ca281
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-01 21:15:55 +0300

    Автоперевірка 1-5

--- auto-check.js
+++ auto-check.js
@@ -1,28 +1,81 @@
 /* Завдання 1 автоперевірка
  */
+// ФУНКЦІЯ ЯК ЗНАЧЕННЯ
+// Доповни код таким чином, щоб у змінній result був результат виконання функції makePizza, 
+// а у змінній pointer було посилання на функцію makePizza.
 //!!!before:
-
-
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// const result = null;
+// const pointer = null;
 //!!!after:
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// const result = makePizza();
+// const pointer = makePizza;
+
 
 
 
 
 /* Завдання 2 автоперевірка
  */
+// КОЛБЕК-ФУНКЦІЇ
+// Доповни функцію makeMessage таким чином, щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала її виклик. 
+// Функція deliverPizza або makePizza буде передаватися як колбек і очікувати аргументом ім'я готової піци, що доставляється.
 //!!!before:
-
-
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+// function makeMessage(pizzaName) {
+//   return;
+// }
 //!!!after:
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+
 
 
 
 /* Завдання 3 автоперевірка
  */
+// ІНЛАЙН-КОЛБЕКИ
+// Доповни другий виклик функції makePizza(pizzaName, callback), 
+// передавши другим аргументом інлайн колбек-функцію eatPizza(pizzaName), яка логує рядок "Eating pizza <назва піци>".
 //!!!before:
-
-
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// makePizza("Ultracheese");
 //!!!after:
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// makePizza("Ultracheese", function deliverPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}.`);
+// });
+
 
 
 
@@ -30,10 +83,50 @@
 
 /* Завдання 4 автоперевірка
  */
+// ДЕКІЛЬКА КОЛБЕКІВ
+// Необхідно написати логіку обробки замовлення піци. Виконай рефакторинг методу order таким чином, 
+// щоб він приймав другим і третім параметром два колбеки onSuccess і onError.
+// Якщо у властивості pizzas відсутня піца з назвою з параметра pizzaName, 
+// метод order повинен повертати результат виклику колбека onError, 
+// передаючи йому аргументом рядок "There is no pizza with a name <имя пиццы> in the assortment."
+// Якщо у властивості pizzas присутня піца з назвою з параметра pizzaName, 
+// метод order повинен повертати результат виклику колбека onSuccess, передаючи йому аргументом назву замовленої піци.
+// Після оголошення об'єкта pizzaPalace ми додали колбеки і виклики методів. Будь ласка, нічого там не міняй.
 //!!!before:
-
-
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName) {},
+// };
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
 //!!!after:
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+//     if (!this.pizzas.includes(pizzaName)) {
+//       return onError( `There is no pizza with a name ${pizzaName} in the assortment.`);
+//     }
+//     return onSuccess(pizzaName);
+//   }
+// };
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
 
 
 
@@ -41,11 +134,26 @@
 
 /* Завдання 5 автоперевірка
  */
+// МЕТОД FOREACH(CALLBACK)
+// Функція calculateTotalPrice(orderedItems) приймає один параметр orderedItems - масив чисел, 
+// і розраховує загальну суму його елементів, яка зберігається у змінній totalPrice і повертається як результат роботи функції.
+// Виконай рефакторинг функції таким чином, щоб замість циклу for, вона використовувала метод forEach.
 //!!!before:
-
-
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   for (let i = 0; i < orderedItems.length; i += 1) {
+//     totalPrice += orderedItems[i];
+//   }
+//   return totalPrice;
+// }
 //!!!after:
-
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   orderedItems.forEach(function (number, index) {
+//   totalPrice += orderedItems[number, index];
+//   });
+//   return totalPrice;
+// }
 
 
 
