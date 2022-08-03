f3a5c27e7ec11c263ff97aaf47aaf38cd07ed65a
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-03 22:14:13 +0300

    Автоперевірка 39-42

--- auto-check.js
+++ auto-check.js
@@ -1057,12 +1057,19 @@
 
 /* Завдання 39 автоперевірка
  */
-
+// СВІЙ ПОРЯДОК СОРТУВАННЯ ЧИСЕЛ
+// Онлайн бібіліотеці необхідно відображати книги, відсортовані за датою видання, за її зростанням або спаданням. 
+// Доповни код таким чином, щоб у змінній ascendingReleaseDates вийшла копія масиву releaseDates, 
+// відсортована за зростанням, а у змінній descendingReleaseDates - копія, відсортована за спаданням.
 //!!!before:
-
-
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const ascendingReleaseDates = releaseDates;
+// const descendingReleaseDates = releaseDates;
 //!!!after:
-  
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
 
 
 
@@ -1070,10 +1077,30 @@
 
 /* Завдання 40 автоперевірка
  */
+// СВІЙ ПОРЯДОК СОРТУВАННЯ РЯДКІВ
+// Онлайн бібіліотеці необхідно відображати книги, відсортовані за автором, в алфавітному і зворотному алфавітному порядку. 
+// Доповни код таким чином, щоб у змінній authorsInAlphabetOrder вийшла копія масиву authors, 
+// відсортована за алфавітом, а у змінній authorsInReversedOrder - копія, відсортована у зворотному алфавітному порядку.
 //!!!before:
-
-
+// const authors = [
+//    "Tanith Lee",
+//    "Bernard Cornwell",
+//    "Robert Sheckley",
+//    "Fyodor Dostoevsky",
+//    "Howard Lovecraft",
+//  ];
+//  const authorsInAlphabetOrder = authors;
+//  const authorsInReversedOrder = authors;
 //!!!after:
+// const authors = [
+//    "Tanith Lee",
+//    "Bernard Cornwell",
+//    "Robert Sheckley",
+//    "Fyodor Dostoevsky",
+//    "Howard Lovecraft",
+//  ];
+//  const authorsInAlphabetOrder = [...authors].sort((a, b) => a.localeCompare(b));;
+//  const authorsInReversedOrder = [...authors].sort((a, b) => b.localeCompare(a));;
   
 
 
@@ -1082,19 +1109,91 @@
 
 /* Завдання 41 автоперевірка
  */
+// СОРТУВАННЯ ОБ'ЄКТІВ
+// Доповни код таким чином, щоб:
+// У змінній sortedByAuthorName вийшов масив книг, відсортований за ім'ям автора в алфавітному порядку.
+// У змінній sortedByReversedAuthorName вийшов масив книг, відсортований за ім'ям автора у зворотному алфавітному порядку.
+// У змінній sortedByAscendingRating вийшов масив книг, відсортований за зростанням рейтингу.
+// У змінній sortedByDescentingRating вийшов масив книг, відсортований за спаданням рейтингу.
 //!!!before:
-
-
+// const books = [
+//    {
+//      title: "The Last Kingdom",
+//      author: "Bernard Cornwell",
+//      rating: 8.38,
+//    },
+//    {
+//      title: "Beside Still Waters",
+//      author: "Robert Sheckley",
+//      rating: 8.51,
+//    },
+//    {
+//      title: "The Dream of a Ridiculous Man",
+//      author: "Fyodor Dostoevsky",
+//      rating: 7.75,
+//    },
+//    { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//    { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//  ];
+//  const sortedByAuthorName = books;
+//  const sortedByReversedAuthorName = books;
+//  const sortedByAscendingRating = books;
+//  const sortedByDescentingRating = books;
 //!!!after:
-
+// const books = [
+//    {
+//      title: "The Last Kingdom",
+//      author: "Bernard Cornwell",
+//      rating: 8.38,
+//    },
+//    {
+//      title: "Beside Still Waters",
+//      author: "Robert Sheckley",
+//      rating: 8.51,
+//    },
+//    {
+//      title: "The Dream of a Ridiculous Man",
+//      author: "Fyodor Dostoevsky",
+//      rating: 7.75,
+//    },
+//    { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//    { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//  ];
+//  const sortedByAuthorName = [...books].sort(
+//    (firstBook, secondBook) => firstBook.author.localeCompare(secondBook.author));
+//  ;
+//  const sortedByReversedAuthorName = [...books].sort(
+//    (firstBook, secondBook) => secondBook.author.localeCompare(firstBook.author));
+//  ;
+//  const sortedByAscendingRating = [...books].sort(
+//    (firstBook, secondBook) => firstBook.rating - secondBook.rating
+//  );
+//  ;
+//  const sortedByDescentingRating = [...books].sort(
+//    (firstBook, secondBook) => secondBook.rating - firstBook.rating
+//  );
+//  ;
  
+
+
+
+
 
 /* Завдання 42 автоперевірка
  */
+// ЗАДАЧА. СОРТУВАННЯ ЗА БАЛАНСОМ
+// Доповни функцію sortByAscendingBalance(users) таким чином, щоб вона повертала масив користувачів, 
+// відсортований за зростанням їх балансу (властивість balance).
 //!!!before:
-
-
+// const sortByAscendingBalance = users => {
+// };
 //!!!after:
+// const sortByAscendingBalance = users => users.sort(
+//    (firstBalance, secondBalance) => firstBalance.balance - secondBalance.balance
+//  );
+
+
+
 
 
 /* Завдання 43 автоперевірка
